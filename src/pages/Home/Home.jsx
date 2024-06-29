5b74444cdf45fe02c96e89b19a94f393d14410f4
renzi-fidele-frontend <renzifidele2001@gmail.com>
2024-06-29 23:00:28 +0200

    Fazendo GET a api das criptomoedas e armazenando no respectivo slice

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,24 +2,43 @@ import { Col, Container, Row } from "react-bootstrap";
 import styles from "./Home.module.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCryptoData } from "../../state/crypto/cryptoSlice";
+
+const fetchOptions = {
+   method: "GET",
+   url: "https://coinranking1.p.rapidapi.com/stats",
+   params: {
+      referenceCurrencyUuid: "yhjMzLPhuIDl",
+   },
+   headers: {
+      "x-rapidapi-key": "69b11e51eamsh115553615181e35p1ed117jsnfc95950b64fd",
+      "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+   },
+};
 
 const Home = () => {
-   const fetchOptions = {
-      method: "GET",
-      url: "https://coinranking1.p.rapidapi.com/stats?referenceCurrencyUuid=yhjMzLPhuIDl",
-      params: {
-         referenceCurrencyUuid: "yhjMzLPhuIDl",
-      },
-      headers: {
-         "x-rapidapi-key": "69b11e51eamsh115553615181e35p1ed117jsnfc95950b64fd",
-         "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-      },
-   };
+   const [loading, setLoading] = useState(false);
+   const { cryptoData } = useSelector((state) => state.crypto);
+   const dispatch = useDispatch();
 
    async function apanharCryptoData() {
-      const res = await axios.request(fetchOptions);
-      console.log(res.data);
+      setLoading(true);
+      let res;
+      try {
+         res = await axios.request(fetchOptions);
+         console.log(res.data);
+         dispatch(setCryptoData(res.data));
+      } catch (error) {
+         console.log(error);
+      }
+      setLoading(false);
    }
+
+   useEffect(() => {
+      if (!cryptoData) apanharCryptoData();
+   }, [cryptoData]);
 
    return (
       <div id={styles.ct}>
