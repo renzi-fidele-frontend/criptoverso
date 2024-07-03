d65cf62f219c177ab143d8757d3b791d5835d77c
renzi-fidele-frontend <renzifidele2001@gmail.com>
2024-07-03 03:39:37 +0200

    Apanhando os detalhes individuais da criptomoeda na página da criptomoeda

--- src/pages/MoedaIndividual/MoedaIndividual.jsx
+++ src/pages/MoedaIndividual/MoedaIndividual.jsx
@@ -1,4 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CryptofetchOptions } from "../../services/cryptoApi";
+
 const MoedaIndividual = () => {
+   const { uuid } = useParams();
+   const [loading, setLoading] = useState(false);
+   const [criptomoeda, setCriptomoeda] = useState(null);
+
+   async function apanharDetalhesCriptomoeda() {
+      setLoading(true);
+      try {
+         const res = await axios.request({ ...CryptofetchOptions, url: `https://coinranking1.p.rapidapi.com/coin/${uuid}` });
+         setCriptomoeda(res.data.data.coin);
+      } catch (error) {
+         console.log(error);
+      }
+      setLoading(false);
+   }
+
+   useEffect(() => {
+      console.log(`O uuid é: ${uuid}`);
+      if (!criptomoeda) apanharDetalhesCriptomoeda();
+   }, [uuid]);
+
    return <div>MoedaIndividual</div>;
 };
 
