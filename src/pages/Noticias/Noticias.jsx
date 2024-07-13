5b6bf4bd8ba03b81ac96594ac09489ddfa7582a5
renzi-fidele-frontend <renzifidele2001@gmail.com>
2024-07-13 22:21:22 +0200

    Reutilizando a paginação na página das corretoras

--- src/pages/Noticias/Noticias.jsx
+++ src/pages/Noticias/Noticias.jsx
@@ -6,6 +6,7 @@ import { setNoticias, setPaginaAtualNoticias, setTotalPaginasNoticias } from "..
 import { useEffect, useState } from "react";
 import { gerarArray } from "../../hooks/useGerarArray";
 import { paginarArray } from "../../hooks/usePaginarArray";
+import Paginacao from "../../components/Paginacao/Paginacao";
 
 const Noticias = () => {
    const [loading, setLoading] = useState(false);
@@ -70,46 +71,17 @@ const Noticias = () => {
 
          {/*  Paginação  */}
          {noticias && (
-            <Row className="mt-2 mb-5 mb-lg-0 pb-5 pb-lg-0">
-               <Col className="mt-md-5">
-                  {/*  Desktop  */}
-                  <Pagination size="lg" className="d-none d-md-flex flex-wrap justify-content-center">
-                     {gerarArray(totalPaginasNoticias)?.map((page, k) => (
-                        <Pagination.Item
-                           onClick={() => {
-                              if (page !== paginaAtualNoticias.current) {
-                                 window.scrollTo({ top: 0, behavior: "smooth" });
-                                 dispatch(setPaginaAtualNoticias(page));
-                                 setNoticiasPaginadas(paginarArray(noticias, page, itemsPorPaginaNoticias));
-                              }
-                           }}
-                           active={page === paginaAtualNoticias}
-                           key={k}
-                        >
-                           {page}
-                        </Pagination.Item>
-                     ))}
-                  </Pagination>
-                  {/*  Mobile  */}
-                  <Pagination size="sm" className="mt-4 mb-5 mb-lg-0 d-flex d-md-none flex-wrap justify-content-center">
-                     {gerarArray(totalPaginasNoticias)?.map((page, k) => (
-                        <Pagination.Item
-                           onClick={() => {
-                              if (page !== paginaAtualNoticias.current) {
-                                 window.scrollTo({ top: 0, behavior: "smooth" });
-                                 dispatch(setPaginaAtualNoticias(page));
-                                 setNoticiasPaginadas(paginarArray(noticias, page, itemsPorPaginaNoticias));
-                              }
-                           }}
-                           active={page === paginaAtualNoticias}
-                           key={k}
-                        >
-                           {page}
-                        </Pagination.Item>
-                     ))}
-                  </Pagination>
-               </Col>
-            </Row>
+            <Paginacao
+               onPageClick={(pagina) => {
+                  window.scrollTo({ top: 0, behavior: "smooth" });
+                  dispatch(setPaginaAtualNoticias(pagina));
+                  setNoticiasPaginadas(paginarArray(noticias, pagina, itemsPorPaginaNoticias));
+               }}
+               paginaAtual={paginaAtualNoticias}
+               tamanhoDesktop="lg"
+               tamanhoMobile="sm"
+               totalPaginas={totalPaginasNoticias}
+            />
          )}
       </Container>
    );
