dce91ae8b3754139c6a004f8fb43b553b129dcb7
Renzi Fidele <renzifidele2001@gmail.com>
2024-08-15 01:38:29 +0200

    Armazenando as carteiras digitais no redux após a requisição

--- src/pages/Carteiras/Carteiras.jsx
+++ src/pages/Carteiras/Carteiras.jsx
@@ -1,19 +1,23 @@
 import axios from "axios";
 import styles from "./Carteiras.module.css";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCarteiras } from "../../state/carteiras/carteirasSlice";
 
 const Carteiras = () => {
+   const { carteiras } = useSelector((state) => state.carteiras);
+   const dispatch = useDispatch();
    async function apanharCarteiras() {
       try {
          const res = await axios(`https://min-api.cryptocompare.com/data/wallets/general?api_key=${import.meta.env.VITE_CRYPTO_WATCH_APIKEY}`);
-         console.log(res.data);
+         dispatch(setCarteiras(res.data.Data));
       } catch (error) {
          console.log(error.message);
       }
    }
 
    useEffect(() => {
-      // apanharCarteiras();
+      if (!carteiras) apanharCarteiras();
    }, []);
 
    return <div>Carteiras</div>;
