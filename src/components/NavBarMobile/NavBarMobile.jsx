47ac0f2e1e13822be5443a8d6cac78ea5c710fff
Renzi Fidele <renzifidele2001@gmail.com>
2024-08-24 17:32:11 +0200

    Adicionando a opção de mudar idioma ao navbar do mobile

--- src/components/NavBarMobile/NavBarMobile.jsx
+++ src/components/NavBarMobile/NavBarMobile.jsx
@@ -1,10 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./NavBarMobile.module.css";
-import { Button, Container, Image, Navbar } from "react-bootstrap";
+import { Button, FormSelect, Image, Navbar } from "react-bootstrap";
 import { setMostrar } from "../../state/MostrarNavBar/MostrarNavBarSlice";
 import { Link } from "react-router-dom";
 import { setModoEscuro } from "../../state/tema/temaSlice";
 import logo from "../../assets/logo1.png";
+import i18n from "../../i18n/i18n";
 
 const NavBarMobile = () => {
    const dispatch = useDispatch();
@@ -17,9 +18,13 @@ const NavBarMobile = () => {
       document.documentElement.setAttribute("data-bs-theme", tema);
    }
 
+   function mudarIdioma(e) {
+      i18n.changeLanguage(e.target.value);
+   }
+
    return (
-      <Navbar className="px-3 d-lg-none d-flex" id={styles.ct} style={{ zIndex: 10 }}>
-         <Container className="container-sm d-flex justify-content-between align-items-center">
+      <Navbar className="d-lg-none d-flex" id={styles.ct} style={{ zIndex: 10 }}>
+         <div className="px-3 px-sm-4 px-md-5 w-100 d-flex justify-content-between align-items-center">
             <div className="d-flex align-items-center">
                <Link to="/" preventScrollReset={false}>
                   <Image src={logo} id={styles.logo} />
@@ -27,6 +32,22 @@ const NavBarMobile = () => {
             </div>
 
             <div className="d-flex gap-2 gap-sm-3 align-items-center">
+               <FormSelect className="d-none d-sm-block border-0 bg-opacity-25 text-light bg-light" role="button" onChange={mudarIdioma}>
+                  <option className="bg-opacity-100 text-dark" value="pt">
+                     <div>🇵🇹 Português</div>
+                  </option>
+                  <option className="bg-opacity-100 text-dark" value="en">
+                     🇺🇸 English
+                  </option>
+               </FormSelect>
+               <FormSelect className="d-sm-none border-0 bg-opacity-25 text-light bg-light" role="button" onChange={mudarIdioma}>
+                  <option className="bg-opacity-100 text-dark" value="pt">
+                     🇵🇹
+                  </option>
+                  <option className="bg-opacity-100 text-dark" value="en">
+                     🇺🇸
+                  </option>
+               </FormSelect>
                <Button onClick={mudarTema} className="rounded-circle" variant="outline-light">
                   <i className={modoEscuro ? "bi bi-brightness-high-fill" : "bi bi-moon-stars-fill"}></i>
                </Button>
@@ -34,7 +55,7 @@ const NavBarMobile = () => {
                   <i className="bi bi-toggles2"></i>
                </Button>
             </div>
-         </Container>
+         </div>
       </Navbar>
    );
 };
